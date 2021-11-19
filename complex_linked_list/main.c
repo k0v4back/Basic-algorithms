f4010957f68ebe2245ad9aa78535321f1bdf754d
Vadim Kosolapov <vadkos33@outlook.com>
2021-11-19 21:46:38 +0300

    Edit create_node() and create free_node()

--- complex_linked_list/main.c
+++ complex_linked_list/main.c
@@ -25,10 +25,6 @@ struct node
     struct node * next;
 };
 
-struct node * current_node;
-struct node * new_node;
-struct node * first_node;
-
 struct node * create_node(uint8_t * format, ...);
 void free_node(struct node * node);
 
@@ -43,11 +39,8 @@ int main(int argc, char **argv)
 
 struct node * create_node(uint8_t * format, ...)
 {
-    struct node * new_node;
-    new_node = (struct node *)malloc(sizeof(struct node));
-
-    if(first_node == NULL)
-        first_node = new_node;
+    struct node * new_node = (struct node *)malloc(sizeof(struct node));
+    struct node * node_ptr = new_node;
 
     new_node->next = NULL;
     new_node->type = _INIT_ELEM;
@@ -55,15 +48,41 @@ struct node * create_node(uint8_t * format, ...)
 
     va_list factor;
     va_start(factor, format);
-
     while(*format) //read each of symbols in loop
     {
+        switch(*format)
+        {
+            case 'd': case 'i': //decimal
+               value.decimal = va_arg(factor, int32_t);
+               new_node = fill_node(new_node, "d", value.decimal);
+               break;
+            case 'r': case 'f': //real
+               value.real = va_arg(factor, double);
+               new_node = fill_node(new_node, "r", value.real);
+               break;
+            case 's':           //string
+               value.string = va_arg(factor, uint8_t *);
+               new_node = fill_node(new_node, "s", value.string);
+              break; 
+        }
 
         ++format;
     }
-
     va_end(factor);
 
 
     return new_node;
 }
+
+void free_node(struct node * node)
+{
+    struct node *node_ptr = NULL;
+
+    while(node != NULL)
+    {
+        node_ptr = node->next;
+        free(node);
+        node = node_ptr;
+    }
+}
+
