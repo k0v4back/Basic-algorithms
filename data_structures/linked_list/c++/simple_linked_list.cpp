11bd17c7d8d73cfe226fc7cc30330ef267a848fd
k0v4 <vadkos33@outlook.com>
2023-11-19 15:33:24 +0300

    Rewrite linked list from C style to C++

--- data_structures/linked_list/c++/simple_linked_list.cpp
+++ data_structures/linked_list/c++/simple_linked_list.cpp
@@ -2,70 +2,93 @@
 
 using namespace std;
 
-#include "simple_linked_list.h"
+template<typename T> class List {
+public:
+    List();
+    ~List();
 
-Person::Person(string name, int age) {
-    this->name = name;
-    this->age = age;
-    this->next = nullptr;
+    void push_back(T data);
+    void print_nodes(void);
+    T& operator[](const int index);
 
+    int GetListSize(void);
+private:
+    template<typename U> class Node {
+    public:
+        U data;
+        Node* next;
+
+        Node(U data = U(), Node* next = nullptr) {
+            this->data = data;
+            this->next = next;
+        }
+    };
+
+    int list_size;
+    Node<T>* first;
+};
+
+
+template<typename T> List<T>::List() {
+    list_size = 0;
+    first = nullptr;
+}
+
+template<typename T> List<T>::~List() {
+
+}
+
+template<typename T> void List<T>::push_back(T data) {
     if (first == nullptr) {
-        first = this;
-        current = first;
+        first = new Node<T>(data);
     } else {
-        new_node = this;
-        current->next = new_node;
-        current = new_node;
+        Node<T>* current = first;
+
+        while (current->next)
+            current = current->next;
+
+        current->next = new Node<T>(data);
     }
+
+    list_size++;
 }
 
-Person::~Person() {
-    cout << "Delete " << name << endl;
-}
-
-string Person::GetName() const {
-    return name;
-}
-
-int Person::GetAge() const {
-    return age;
-}
-
-class Person* Person::GetNext() const {
-    return next;
-}
-
-void Person::SetName(string name) {
-    this->name = name;
-}
-
-void Person::SetAge(int age) {
-    this->age = age;
-}
-
-void Person::SetNext(class Person* next) {
-    this->next = next;
-}
-
-void Person::PrintNode(void) {
-    cout << "Name: " << name << " " << "Age: " << age << endl;
-}
-
-void Person::PrintAllNodes(void) {
-    current = first;
+template<typename T> void List<T>::print_nodes(void) {
+    Node<T>* current = first;
 
     while (current) {
-        current->PrintNode();
+        cout << current->data << endl;
         current = current->next;
     }
 }
 
-int main() {
-    Person* p1 = new Person("Oliver", 30);
-    Person* p2 = new Person("Arthur", 31);
-    Person* p3 = new Person("Henry", 32);
+template<typename T> T& List<T>::operator[](const int index) {
+    Node<T>* current = first;
+    int i = 0;
 
-    Person::PrintAllNodes();
+    while(current) {
+        if (i == index)
+            return current->data;
+        current = current->next;
+        i++;
+    }
+}
+
+template<typename T> int List<T>::GetListSize(void) {
+    return list_size;
+}
+
+
+int main(void) {
+    List<int> list;
+
+    list.push_back(1);
+    list.push_back(2);
+    list.push_back(3);
+
+    cout << list[1] << endl;
+    cout << "----------------------" << endl;
+    list.print_nodes();
 
     return 0;
 }
